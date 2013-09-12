e8dd4ca8bd51b84d5d7d5a6a1c4144475e066bf1
Supasate Choochaisri <supasate@larngeartech.com>
2013-09-12 16:15:03 +0700

    Create do_requestion function to be used by other methods.

--- zabbix.py
+++ zabbix.py
@@ -1,5 +1,8 @@
 import requests
 
+class ZabbixError(Exception):
+    pass
+
 class Api(object):
     def __init__(self, server='http://localhost/zabbix'):
         self.session = requests.Session()
@@ -9,4 +12,22 @@ class Api(object):
         self.url = server + '/api_jsonrpc.php'
         self.auth = ''
         self.id = 0
+
+    def do_request(self, method, params=None):
+        json_payload = {
+            'jsonrpc': '2.0',
+            'method': method,
+            'params': params or {},
+            'auth': self.auth,
+            'id': self.id,
+        }
+        self.id += 1
+        response = self.session.post(self.url, data = json.dumps(json_payload))
+
+        if response.status_code != 200:
+            raise ZabbixError("HTTP ERROR %S: %S" % (response.status, response.reason))
+        if response.text == '':
+            raise ZabbixError("Received empty response")
+
+        return response.json()
 
