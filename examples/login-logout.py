f36b84e26f1bcbcb970fcddddd3cdecaffdac420
Supasate Choochaisri <supasate@larngeartech.com>
2013-09-12 16:50:39 +0700

    Add login-logout example

---
+++ examples/login-logout.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('../')
+
+import zabbix
+
+config = {}
+execfile("config.py", config)
+
+server = config["server"]
+username = config["user"]
+password = config["password"]
+
+api = zabbix.Api(server)
+api.login(username, password)
+api.logout()
